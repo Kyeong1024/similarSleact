a845ab2a06dbe49c9ce39b6309a021f031a287e2
Kyeong1024 <aofldjxm@gmail.com>
2022-09-25 10:24:12 +0900

    Delete: js file

--- src/common/decorators/token.decorator.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Token = void 0;
-var common_1 = require("@nestjs/common");
-exports.Token = (0, common_1.createParamDecorator)(function (data, ctx) {
-    var response = ctx.switchToHttp().getResponse();
-    return response.locals.jwt;
-});
