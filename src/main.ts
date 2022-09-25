5674e34834e10f14b730d4ef0eb0ca8c26f4ef08
Kyeong1024 <aofldjxm@gmail.com>
2022-09-25 13:46:04 +0900

    Add: express middleware

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import passport from 'passport';
+import session from 'express-session';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './filters/http-exception.filter';
 
@@ -9,6 +10,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.use(
+    session({
+      resave: false,
+      saveUninitialized: false,
+      secret: process.env.COOKIE_SECRET,
+      cookie: {
+        httpOnly: true,
+      },
+    }),
+  );
   app.use(passport.initialize());
   app.use(passport.session());
   const config = new DocumentBuilder()
