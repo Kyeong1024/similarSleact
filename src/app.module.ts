784be7210dec39568f2a7b93beb2c535d82a6a14
Kyeong1024 <aofldjxm@gmail.com>
2022-10-02 22:55:42 +0900

    Add: setting socket io

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { Mentions } from './entities/Mentions';
 import { WorkspaceMembers } from './entities/WorkspaceMembers';
 import { Workspaces } from './entities/Workspaces';
 import { AuthModule } from './auth/auth.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
   imports: [
@@ -49,6 +50,7 @@ import { AuthModule } from './auth/auth.module';
     ChannelsModule,
     WorkspacesModule,
     AuthModule,
+    EventsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/events/events.gateway.ts
@@ -0,0 +1,58 @@
+import {
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+  MessageBody,
+  ConnectedSocket,
+} from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { onlineMap } from './onlineMap';
+
+@WebSocketGateway({ namespace: /\/ws-.+/ })
+export class EventsGateway
+  implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
+{
+  @WebSocketServer() public server: Server;
+
+  @SubscribeMessage('test')
+  handleTest(client: any, payload: any): string {
+    return 'test~~';
+  }
+
+  @SubscribeMessage('login')
+  handleLogin(
+    @MessageBody() data: { id: number; channels: number[] },
+    @ConnectedSocket() socket: Socket,
+  ) {
+    const newNamespace = socket.nsp;
+    onlineMap[socket.nsp.name][socket.id] = data.id;
+    newNamespace.emit('onlineList', Object.values(onlineMap[socket.nsp.name]));
+    data.channels.forEach((channel) => {
+      console.log('channel ==>', channel);
+      socket.join(`${socket.nsp.name}-${channel}`);
+    });
+  }
+
+  afterInit(server: any) {
+    console.log('init');
+  }
+
+  handleConnection(@ConnectedSocket() socket: Socket) {
+    console.log('connected ', socket.nsp.name);
+
+    if (!onlineMap[socket.nsp.name]) {
+      onlineMap[socket.nsp.name] = {};
+    }
+
+    socket.emit('hello', socket.nsp.name);
+  }
+
+  handleDisconnect(@ConnectedSocket() socket: Socket) {
+    const newNamespace = socket.nsp;
+    delete onlineMap[socket.nsp.name][socket.id];
+    newNamespace.emit('onlineList', Object.values(onlineMap[socket.nsp.name]));
+  }
+}
